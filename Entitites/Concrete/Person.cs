3ba93ccbd1424deccb1abea4230d673bd02f51a2
DursuncanAydemir <dursuncan.aydemir28@gmail.com>
2024-02-28 15:27:35 +0300

    Add project files.

---
+++ Entitites/Concrete/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entitites.Concrete
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public long NationalIdentity { get; set; }
+
+        public int DateOfBirthYear { get; set; }
+    }
+}
